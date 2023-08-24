fa72512ad1ea1416680dd966e6ba39668b276b9a
Skythrew <mael.guerin023@gmail.com>
2023-08-24 09:52:59 +0200

    feat: add configuration menu

--- stage-generation/gen.sh
+++ stage-generation/gen.sh
@@ -9,22 +9,54 @@ if [ "$UID" != "0" ]; then
     exit 1
 fi
 
-echo "====================================================================="
-echo "                   CAMBRIA STAGE GENERATION TOOL                     "
-echo "====================================================================="
-echo ""
-echo "[1] BASE"
-echo ""
+conf_menu() {
+    echo "====================================================================="
+    echo "                         CONFIGURATION                               "
+    echo "====================================================================="
+    echo ""
+    echo "[1] Enable parallel builds"
+    echo "[?] Show current configuration"
+    echo ""
+    read -p "Enter your choice: " CHOICE
+    if [ "$CHOICE" == "1" ]; then
+        PARALLEL_BUILD=1
+        read -p "How many build jobs do you want ? " PARALLEL_JOBS
+        clear
+        conf_menu
+    elif [ "$CHOICE" == "?" ]; then
+        echo "PARALLEL_BUILD=$PARALLEL_BUILD"
+        sleep 3
+        clear
+        menu
+    else
+        clear
+        menu
+    fi
+}
 
-read -p "Enter your choice: " CHOICE
+menu() {
+    echo "====================================================================="
+    echo "                   CAMBRIA STAGE GENERATION TOOL                     "
+    echo "====================================================================="
+    echo ""
+    echo "[1] BASE"
+    echo ""
+    echo "[C] Configuration"
+    echo ""
+    read -p "Enter your choice: " CHOICE
 
-if [ "$CHOICE" == "1" ]; then
-    source stage-generation/base.sh
-    build
-else
-    echo "Invalid choice. Exiting..."
-    exit 1
-fi
+    if [ "$CHOICE" == "1" ]; then
+        source stage-generation/base.sh
+        build
+    elif [ "$CHOICE" == "c" ] || [ "$CHOICE" == "C" ]; then
+        clear
+        conf_menu
+    else
+        echo "Invalid choice. Exiting..."
+        exit 1
+    fi
+}
 
+menu
 clean_cache
 compress_build
