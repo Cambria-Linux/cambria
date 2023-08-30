a6a71d597133be4745c1713bd480accb7d5554a8
Skythrew <mael.guerin023@gmail.com>
2023-08-30 10:59:24 +0200

    feat: make conf_menu use gum

--- stage-generation/gen.sh
+++ stage-generation/gen.sh
@@ -11,35 +11,40 @@ if [ "$UID" != "0" ]; then
 	exit 1
 fi
 
+print_choices() {
+	for c in $1; do
+		echo $C
+	done
+}
+
 conf_menu() {
 	echo "====================================================================="
 	echo "                         CONFIGURATION                               "
 	echo "====================================================================="
 	echo ""
-	echo "[1] Enable parallel builds"
-	echo "[2] MAKEOPTS configuration"
-	echo "[3] Base stage choice"
-	echo "[?] Show current configuration"
+	CHOICES=("[1] Emerge jobs")
+	CHOICES+=("[2] Build jobs")
+	CHOICES+=("[3] Base stage choice")
+	CHOICES+=("[4] Show current configuration")
 	echo ""
-	read -p "Enter your choice: " CHOICE
-	if [ "$CHOICE" == "1" ]; then
+	CHOICE=$(printf '%s\n' "${CHOICES[@]}" | gum choose)
+	if [[ "$CHOICE" == "[1]"* ]]; then
 		PARALLEL_BUILD=1
-		read -p "How many build jobs do you want ? " PARALLEL_JOBS
+		PARALLEL_JOBS=$(eval "gum choose {1..$(lscpu --all --parse=CORE,SOCKET | grep -Ev "^#" | sort -u | wc -l)}")
 		clear
 		conf_menu
-	elif [ "$CHOICE" == "2" ]; then
-		read -p "Please enter your MAKEOPTS: " MAKEOPTS
+	elif [[ "$CHOICE" == "[2]"* ]]; then
+		MAKEJOBS=$(eval "gum choose {1..$(nproc)}")
 		clear
 		conf_menu
-	elif [ "$CHOICE" == "3" ]; then
-		read -p "What's the name of your base stage file ? " BASE_STAGE
-		BASE_STAGE=$PWD/$BASE_STAGE
+	elif [[ "$CHOICE" == "[3]"* ]]; then
+		BASE_STAGE=$PWD/$(for file in *.tar.xz; do echo $file; done | gum choose)
 		clear
 		conf_menu
-	elif [ "$CHOICE" == "?" ]; then
+	elif [[ "$CHOICE" == "[4]"* ]]; then
 		echo "PARALLEL_BUILD=$PARALLEL_BUILD"
 		echo "PARALLEL_JOBS=$PARALLEL_JOBS"
-		echo "MAKEOPTS=$MAKEOPTS"
+		echo "MAKEJOBS=$MAKEJOBS"
 		echo "BASE_STAGE=$BASE_STAGE"
 		sleep 3
 		clear
@@ -48,6 +53,7 @@ conf_menu() {
 		clear
 		stage_menu
 	fi
+	exit
 }
 
 menu() {
