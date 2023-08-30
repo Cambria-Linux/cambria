b5f74fd740e48e24b9773f45144bac843fbede03
camini <camini57@gmail.com>
2023-08-30 06:27:52 +0200

    feat : add discord

--- stage-generation/gnome.sh
+++ stage-generation/gnome.sh
@@ -31,13 +31,16 @@ build() {
 emerge-webrsync
 
 emerge --sync --quiet
-emerge -quDN @world
+
 EOF
 
 	echo "gnome-extra/evolution-data-server ~amd64" >>etc/portage/package.accept_keywords/evolution-data-server
 	echo "media-gfx/gnome-photos ~amd64" >>etc/portage/package.accept_keywords/gnome-photos
     echo "gui-apps/gnome-console ~amd64" >>etc/portage/package.accept_keywords/gnome-console
     echo "x11-libs/libdrm ~amd64" >>etc/portage/package.accept_keywords/libdrm
+	echo "gnome-extra/gnome-software ~amd64" >>etc/portage/package.accept_keywords/gnome-software
+	echo "net-im/discord ~amd64" >>etc/portage/package.accept_keywords/discord
+	
     echo "x11-libs/libdrm video_cards_intel" >>etc/portage/package.use/libdrm
     echo "media-libs/libsndfile minimal" >>etc/portage/package.use/libsndfile
 	echo "media-libs/libmediaart -gtk" >>etc/portage/package.use/libmediaart
@@ -47,8 +50,19 @@ EOF
 	echo "media-libs/gegl raw" >>etc/portage/package.use/gegl
 	echo "media-libs/gst-plugins-base theora" >>etc/portage/package.use/gst-plugins-base
 	echo "media-plugins/grilo-plugins tracker" >>etc/portage/package.use/grilo-plugins
+	echo "gnome-extra/gnome-software flatpak" >>etc/portage/package.use/gnome-software
+	echo "net-dns/avahi -gtk -qt5" >>etc/portage/package.use/avahi
 
-	install_packages sys-apps/flatpak gnome-browser-connector gnome-tweaks gnome-extra/mousetweaks evince gnome-contacts totem gnome-keyring gnome-text-editor gnome-calendar gnome-maps gnome-weather gnome-music cheese baobab gnome-disk-utility gnome-photos gjs gnome-control-center gnome-core-libs gnome-session gnome-settings-daemon gnome-shell gvfs nautilus cantarell gnome-console adwaita-icon-theme gnome-backgrounds gnome-themes-standard mutter firefox-bin thunderbird-bin eog 
+	cat <<EOF | chroot .
+emerge -quDN @world
+EOF
+
+	install_packages discord media-fonts/noto-emoji gnome-software sys-apps/flatpak gnome-browser-connector gnome-tweaks gnome-extra/mousetweaks evince gnome-contacts totem gnome-keyring gnome-text-editor gnome-calendar gnome-maps gnome-weather gnome-music cheese baobab gnome-disk-utility gnome-photos gjs gnome-control-center gnome-core-libs gnome-session gnome-settings-daemon gnome-shell gvfs nautilus cantarell gnome-console adwaita-icon-theme gnome-backgrounds gnome-themes-standard mutter firefox-bin thunderbird-bin eog 
 	enable_services gdm NetworkManager bluetooth avahi-daemon cups
+
+	cat <<EOF | chroot .
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+EOF
+
     unmount_chroot
 }
