ffc9e9168c5b62774377fdc902ad2f9250c4d701
Skythrew <mael.guerin023@gmail.com>
2023-08-31 11:18:28 +0200

    feat: finish kde stage

--- stage-generation/kde.sh
+++ stage-generation/kde.sh
@@ -16,4 +16,42 @@ build() {
     print_info "Extracting base stage..."
 	extract_stage $BASE_STAGE
 	print_success "Done !"
+
+    print_info "Writing portage configuration..."
+    USEFLAGS="kde -gnome egl X gles2 x264 x265 v4l grub zeroconf cups bluetooth vulkan pipewire wayland networkmanager pulseaudio" configure_portage
+    print_success "Done !"
+
+    print_info "Setting DNS info..."
+	set_dns
+	print_success "Done !"
+
+    print_info "Building $STAGE stage..."
+	setup_chroot
+    cat <<EOF | chroot .
+emerge-webrsync
+
+emerge --sync --quiet
+
+EOF
+
+    echo "x11-libs/libdrm video_cards_intel" >>etc/portage/package.use/libdrm
+    echo "media-libs/libsndfile minimal" >>etc/portage/package.use/libsndfile
+    echo "net-dns/avahi -gtk -qt5" >>etc/portage/package.use/avahi
+	echo "net-wireless/wpa_supplicant -qt5" >>etc/portage/package.use/wpa_supplicant
+	echo "app-text/mupdf -opengl -X" >>etc/portage/package.use/mupdf
+    echo "kde-plasma/plasma-meta discover display-manager sddm wallpapers" >>etc/portage/package.use/kde
+    echo "kde-plasma/discover flatpak" >>etc/portage/package.use/discover
+    echo "kde-apps/kde-apps-meta accessibility -admin -education -games graphics multimedia -network -pim -sdk utils" >>etc/portage/package.use/kde-apps
+
+    cat <<EOF | chroot .
+emerge -quDN @world
+EOF
+
+    install_packages kde-plasma/plasma-meta kde-apps/kde-apps-meta www-client/firefox-bin mail-client/thunderbird-bin
+    enable_services sddm NetworkManager bluetooth avahi-daemon cups
+
+    cat <<EOF | chroot .
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+EOF
+    unmount_chroot
 }
