2c2412cfc49890ebf5026a03ee0996519619e1e8
Skythrew <mael.guerin023@gmail.com>
2023-09-01 10:44:09 +0200

    feat(cambria-center): add locale configuration

--- post-install/cambria-center.sh
+++ post-install/cambria-center.sh
@@ -4,6 +4,18 @@
 # All-in-one script to manage post-install operations.
 #===================================================
 
+print_info() {
+	echo -e "\e[1;36m$1\e[0m"
+}
+
+print_err() {
+	echo -e "\e[1;31m$1\e[0m"
+}
+
+print_success() {
+	echo -e "\e[1;32m$1\e[0m"
+}
+
 gum_menu() {
 	i=1
 	CHOICES=()
@@ -14,4 +26,33 @@ gum_menu() {
 	CHOICE=$(printf '%s\n' "${CHOICES[@]}" | gum choose)
 }
 
-gum_menu "Language configuration" "GDM AZERTY" "Clean system (takes some while)" "System optimizer (takes some while)"
+configure_locale() {
+	echo "$LOCALE.UTF-8 UTF-8" >> /etc/locale.gen
+	locale-gen
+	cat <<EOF > /etc/locale.conf
+LANG="$LOCALE.UTF-8"
+LC_COLLATE="C.UTF-8"
+EOF
+}
+
+menu() {
+	gum_menu "Language configuration" "GDM AZERTY" "Clean system (takes some while)" "System optimizer (takes some while)" "Reboot (needed to apply changes)" "Exit"
+
+	# Locale menu
+	if [[ "$CHOICE" == "[1]"* ]]; then
+		LOCALE=$(grep "UTF-8" /usr/share/i18n/SUPPORTED | awk '{print $1}' | sed 's/^#//;s/\.UTF-8//' | gum filter --limit 1 --header "Choose your locale:")
+		clear
+		print_info "Configuring locale..."
+		configure_locale
+		print_success "Done !"
+	fi
+
+	if [[ "$CHOICE" == "[6]"* ]]; then
+		exit
+	fi
+
+	clear
+	menu
+}
+
+menu
